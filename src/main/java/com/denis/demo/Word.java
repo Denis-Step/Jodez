e241edc073b396f4fbb27096899602bdb8223874
Denis Stepanenko <cheerios4cat@gmail.com>
2021-03-15 23:59:01 -0400

    Have to halt working on Jedis, making Models for ORM

--- src/main/java/com/denis/demo/Word.java
+++ src/main/java/com/denis/demo/Word.java
@@ -1,4 +1,28 @@
 package com.denis.demo;
 
 public class Word {
+    public String word;
+    public int wordID;
+
+    public Word(String word, int wordID){
+        this.word = word;
+        this.wordID = wordID;
+    }
+
+    public String getWord(){
+        return this.word;
+    }
+
+    public void setWord(String word){
+        this.word = word;
+    }
+
+    public int getWordID(){
+        return this.wordID;
+    }
+
+    public void setWordID(int wordID){
+        this.wordID = wordID;
+    }
+
 }
